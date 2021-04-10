7c0a3d0d18b4afb8ddbd6e6eb8a192c9e53eb843
ivanlong20 <ivan.kaalong@gmail.com>
2021-04-10 17:44:30 +0800

    Modified the template

--- app/src/main/java/com/example/dietplan/BMIFragment.java
+++ app/src/main/java/com/example/dietplan/BMIFragment.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
 public class BMIFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable
--- app/src/main/java/com/example/dietplan/BMRFragment.java
+++ app/src/main/java/com/example/dietplan/BMRFragment.java
@@ -14,5 +14,6 @@ public class BMRFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable
             ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_bmr, null);
+
     }
 }
