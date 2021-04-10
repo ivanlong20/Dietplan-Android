db16db80050276e9ee624b706501e4ea6b74525b
ivanlong20 <ivan.kaalong@gmail.com>
2021-04-10 17:14:47 +0800

    Modified the template

--- app/src/main/java/com/example/dietplan/MainActivity.java
+++ app/src/main/java/com/example/dietplan/MainActivity.java
@@ -1,30 +1,55 @@
 package com.example.dietplan;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.example.dietplan.ui.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+        implements
+        BottomNavigationView.OnNavigationItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+//loading the default fragment
+        loadFragment(new HomeFragment());
+//getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation =
+                findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
-
+    private boolean loadFragment(Fragment fragment) {
+//switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_bmr:
+                fragment = new BMRFragment();
+                break;
+            case R.id.navigation_bmi:
+                fragment = new BMIFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
 }
+
