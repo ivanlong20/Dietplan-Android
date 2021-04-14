4ed23d9194f3a32707aa6d8703829230b2fa4ac9
E-20-8-1-N <E-20-8-1-N>
2021-04-15 03:06:37 +0800

    Updated 1800 all-days

--- app/src/main/java/com/example/dietplan/Dietplan1800Day1.java
+++ app/src/main/java/com/example/dietplan/Dietplan1800Day1.java
@@ -2,16 +2,25 @@ package com.example.dietplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class Dietplan1800Day1 extends AppCompatActivity {
+public class Dietplan1800Day1 extends AppCompatActivity implements View.OnClickListener {
+    private ImageButton imageButton1,  imageButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dietplan1800_day1);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageButton1 = findViewById(R.id.ib11800d1);
+        imageButton1.setOnClickListener(this);
+        imageButton2 = findViewById(R.id.ib21800d1);
+        imageButton2.setOnClickListener(this);
     }
     @Override
     public boolean onOptionsItemSelected (MenuItem item){
@@ -24,6 +33,19 @@ public class Dietplan1800Day1 extends AppCompatActivity {
 
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ib11800d1: {
+                Intent i = new Intent(Dietplan1800Day1.this, Dietplan1800Day7.class);
+                startActivity(i);
+                break; }
+            case R.id.ib21800d1: {
+                Intent i = new Intent(Dietplan1800Day1.this, Dietplan1800Day2.class);
+                startActivity(i);
+                break; }
         }
     }
 }
