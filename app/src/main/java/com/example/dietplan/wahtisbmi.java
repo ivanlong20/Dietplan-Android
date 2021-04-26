e896c606f870afbf64e5516d6339bfb1fb1617d9
ivanlong20 <ivan.kaalong@gmail.com>
2021-04-27 00:04:58 +0800

    fix

--- app/src/main/java/com/example/dietplan/wahtisbmi.java
+++ app/src/main/java/com/example/dietplan/wahtisbmi.java
@@ -2,9 +2,12 @@ package com.example.dietplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.icu.text.DisplayContext;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class wahtisbmi extends AppCompatActivity {
 
@@ -12,6 +15,32 @@ public class wahtisbmi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wahtisbmi);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("What is BMI ?");
 
 }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.bottom_nav_menu2, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:{
+                // todo: goto back activity from here
+
+                finish();
+                return true;
+            }
+            case R.id.navigation_home:{
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
